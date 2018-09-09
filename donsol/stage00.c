891f736814c4930fd3fd5acb9e23b9d4886a6300
Xoorath <jared@xoorath.com>
2018-09-08 22:56:03 -0400

    adding makemask, moving data to correct segment

--- donsol/stage00.c
+++ donsol/stage00.c
@@ -9,7 +9,8 @@
 #include "main.h"
 #include "render.h"
 
-#include "gen.n6401.h"
+//#include "gen.n6401.h"
+extern unsigned short ani012[];
 
 static float triPos_x; /* The display position, X */
 static float triPos_y; /* The display position, Y */
@@ -21,16 +22,19 @@ static float theta;  /* The rotational angle of the square  */
 #define VTX_UV(u, v) u, v
 #define VTX_RGBA(r, g, b, a) r, g, b, a
 static Vtx shade_vtx[] =  {
-        {VTX_POS(-64,  64, -5), 0, VTX_UV(0,    2048),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
-        {VTX_POS( 64,  64, -5), 0, VTX_UV(2048, 2048),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
-        {VTX_POS( 64, -64, -5), 0, VTX_UV(2048, 0   ),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
-        {VTX_POS(-64, -64, -5), 0, VTX_UV(0,    0   ),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+        {VTX_POS(-64,  64, -5), 0, VTX_UV(0,    4096),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+        {VTX_POS( 64,  64, -5), 0, VTX_UV(4096, 4096),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+        {VTX_POS( 64, -64, -5), 0, VTX_UV(4096, 0   ),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+
+        {VTX_POS( 64, -65, -5), 0, VTX_UV(4096, 0   ),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+        {VTX_POS(-64, -65, -5), 0, VTX_UV(0,    0   ),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) },
+        {VTX_POS(-64,  63, -5), 0, VTX_UV(0,    4096),   VTX_RGBA(0xff, 0xff, 0xff, 0xff) }
 };
 #undef VTX_POS
 #undef VTX_UV
 #undef VTX_RGBA
 
-static u16 squareIndicies[] = {0,1,2, 2,3,0};
+static u16 squareIndicies[] = {0,1,2, 3,4,5};
 
 static Mesh_t squareMesh = {
   .vtx = shade_vtx,
@@ -43,14 +47,14 @@ extern u8 _gentexSegmentRomStart[];
 extern u8 _gentexSegmentRomEnd[];
 
 void DrawTexture();
-//void ReadTexture();
+void ReadTexture();
 /* The initialization of stage 0  */
 void initStage00(void)
 {
   triPos_x = 0.0;
   triPos_y = 0.0;
   theta = 0.0;
-  //ReadTexture();
+  ReadTexture();
   
 }
 
@@ -62,11 +66,11 @@ void makeDL00(void)
     guTranslate(&g_RenderFrameData->translation, triPos_x, triPos_y, 0.0F);
     guRotate(&g_RenderFrameData->rotation, theta, 0.0F, 0.0F, 1.0F);
 
-//    { Render_BeginDraw();
-//      Render_SetCycleInfo(&g_DefaultCycleInfo);
-//      Render_SetGeometryMode(G_SHADE | G_SHADING_SMOOTH);
-//      Render_Mesh(&squareMesh);
-//    } Render_EndDraw();
+    //{ Render_BeginDraw();
+    //  Render_SetCycleInfo(&g_DefaultCycleInfo);
+    //  Render_SetGeometryMode(G_SHADE | G_SHADING_SMOOTH);
+    //  Render_Mesh(&squareMesh);
+    //} Render_EndDraw();
 
     guTranslate(&g_RenderFrameData->translation, triPos_x, triPos_y, 0.0F);
     guRotate(&g_RenderFrameData->rotation, 0.f, 0.0F, 0.0F, 1.0F);
@@ -81,55 +85,37 @@ void makeDL00(void)
   } Render_EndFrame();
 }
 
-// unsigned short MovieBuf[8200];
+unsigned short MovieBuf[8200];
 
-// void ReadTexture()
-// {
+void ReadTexture()
+{
 
-//     u32 r2rlen;
-//     u32 onecutlen;
+  u32 r2rlen;
+  u32 onecutlen;
 
-//     /* Compute the stored segment heads of the movie data */
-//     r2rlen = _gentexSegmentRomEnd - _gentexSegmentRomStart;
+  /* Compute the stored segment heads of the movie data */
+  r2rlen = (u32)_gentexSegmentRomEnd - (u32)_gentexSegmentRomStart;
 
-//     /* Compute the length of each scene */
-//     onecutlen = r2rlen;
+  /* Compute the length of each scene */
+  onecutlen = r2rlen;
 
-//     /* Copy data from ROM to RDRAM, practically */
-//     nuPiReadRom( (u32)(_gentexSegmentRomStart + onecutlen),
-// 		(void *)&(MovieBuf[0]),
-// 		onecutlen
-// 		);
-// }
-/**
- * 
- *  *   Size: 64 x 64
- *   Number of channels: 3
- *   Number of bits per texel: 16 (G_IM_SIZ_16b)
- *   Format of texel: G_IM_FMT_RGBA
- * 
- *  *   gsDPPipeSync (),
- *   gsDPSetCombineMode (G_CC_DECALRGB, G_CC_DECALRGB),
- *   gsDPSetTexturePersp (G_TP_PERSP),
- *   gsDPSetTextureLOD (G_TL_TILE),
- *   gsDPSetTextureFilter (G_TF_BILERP),
- *   gsDPSetTextureConvert(G_TC_FILT),
- *   gsDPSetTextureLUT (G_TT_NONE),
- *   gsDPLoadTextureBlock (ani01, G_IM_FMT_RGBA, G_IM_SIZ_16b, 64, 64, 0,
- *     G_TX_WRAP | G_TX_NOMIRROR, G_TX_WRAP | G_TX_NOMIRROR,
- *     G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD),
-*/
+  /* Copy data from ROM to RDRAM, practically */
+  nuPiReadRom((u32)(_gentexSegmentRomStart),
+              (void *)MovieBuf,
+              onecutlen);
+}
+
 void DrawTexture() {
+
   gDPSetTextureFilter(GFX(), G_TF_POINT /*G_TF_BILERP*/);
   gDPSetCombineMode(GFX(), G_CC_DECALRGB, G_CC_DECALRGB);
   gDPSetTexturePersp(GFX(), G_TP_PERSP);
   gDPSetTextureLOD(GFX(), G_TL_TILE);
-  gSPTexture(GFX(), 0xffff, 0xffff, 0, G_TX_RENDERTILE, G_ON);
-  gDPLoadTextureBlock(GFX(), ani012, G_IM_FMT_RGBA, G_IM_SIZ_16b, 64, 64, 0,
+  gSPTexture(GFX(), 0x8000, 0x8000, 0, G_TX_RENDERTILE, G_ON);
+  gDPLoadTextureBlock(GFX(), &MovieBuf[4], G_IM_FMT_RGBA, G_IM_SIZ_16b, 64, 64, 0,
     G_TX_WRAP | G_TX_NOMIRROR, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD);
 
 }
-
 
 /* The game progressing process for stage 0 */
 void updateGame00(void)
